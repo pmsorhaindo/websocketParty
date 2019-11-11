d48a25c0e012e1091b015eb8f59976bf9f765f0f
Michael Sorhaindo <michael@exmachina.nl>
2019-11-11 14:26:47 +0100

    pass messages from stdin from the client

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const WebSocket = require('ws');
+const split = require('split');
 
 const ws = new WebSocket('ws://localhost:8080/test');
 
@@ -10,3 +11,5 @@ ws.on('message', function incoming(data) {
   console.log(`server said: ${data}`);
   setTimeout(() => ws.send(`val: ${Math.random()}`), 2000);
 });
+
+process.stdin.pipe(split()).on('data', (msg) => ws.send(msg));
--- server.js
+++ server.js
@@ -2,16 +2,46 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+const clients = {};
+
+const generateNumberBetween = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const generateId = () => {
+  const MINIMUM_CLIENT_ID = 10000;
+  const MAXIMUM_CLIENT_ID = 99999;
+  return generateNumberBetween(MINIMUM_CLIENT_ID, MAXIMUM_CLIENT_ID);
+}
+
+const generateColor = () => {
+  const colorVal = () => generateNumberBetween(0,255);
+  return `rgb(${colorVal()}, ${ colorVal()}, ${colorVal()})`;
+}
+
+const generateInitialClient = (id) => ({
+  id,
+  color: generateColor(),
+  position: {
+    x: 0,
+    y: 0,
+  },
+});
+
+const generateNewClientId = () => {
+  let id = generateId();
+  if (Object.keys(clients).includes(id)) return generateNewClientId();
+  clients[id] = generateInitialClient(id);
+  return id;
+}
+
 wss.on('connection', function connection(ws, req) {
-  const ip = req.connection.remoteAddress;
+  const clientId = generateNewClientId();
   ws.on('message', function incoming(message) {
-    if (message === 'initiating!') console.log('received client initiation request');
-    console.log('received: %s', message);
+    console.log('received:', message, clientId);
 
-    setTimeout(() => ws.send(`val: ${Math.random()}`), 2000);
+    setTimeout(() => ws.send(JSON.stringify(clients)), 2000);
   });
-
-
 });
 
 console.log('Server up and running on port: 8080');
